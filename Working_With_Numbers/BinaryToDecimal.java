41db2c06c975ee3e83b3921dd007699fb9f00f49
harish96880 <sriharishr105@gmail.com>
2023-08-23 16:33:17 +0530

    Adding BinaryToDecimalFile

--- Working_With_Numbers/BinaryToDecimal.java
+++ Working_With_Numbers/BinaryToDecimal.java
@@ -1,5 +1,18 @@
 package Working_With_Numbers;
 
-public class BinaryToDecimal {
+import java.util.Scanner;
 
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int binary = sc.nextInt();
+        int decimal = 0;
+        int n = 0;
+        while(binary > 0) {
+            if (binary % 10 == 1) decimal += Math.pow(2, n);
+            binary /= 10;
+            n++;
+        }
+        System.out.println(decimal);
+    }
 }
